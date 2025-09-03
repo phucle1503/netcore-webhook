eba4a7149b98f7a9870267147e05061cc9a36aa5
phucle1503 <phuc.le@akadigital.vn>
2025-09-03 13:53:15 +0700

    Add GET route for Netcore validation

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // parse x-www-form-urlencoded
 
 app.post('/webhook/netcore', (req, res) => {
   console.log('📩 Webhook data received from Netcore:');
   console.log(req.body);
+  console.log('Headers:', req.headers);
   res.status(200).send('Webhook received');
 });
 
